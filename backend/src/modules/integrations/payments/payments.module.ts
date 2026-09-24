b700de6c1a5212ce528cab6dbc5ddde9a926cb1f
toxa2210 <artikovtohir2210@gmail.com>
2026-09-24 20:38:00 +0500

    feat(payments): centralize payment lifecycle

--- backend/src/modules/integrations/payments/payments.module.ts
+++ backend/src/modules/integrations/payments/payments.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { ClickService } from "./click.service";
 import { PaymeService } from "./payme.service";
 import { PaymentsController } from "./payments.controller";
+import { PaymentsService } from "./payments.service";
 import { PaynetController } from "./paynet.controller";
 import { PaynetService } from "./paynet.service";
 
 @Module({
   controllers: [PaymentsController, PaynetController],
-  providers: [ClickService, PaymeService, PaynetService],
-  exports: [ClickService, PaymeService, PaynetService]
+  providers: [PaymentsService, ClickService, PaymeService, PaynetService],
+  exports: [PaymentsService, ClickService, PaymeService, PaynetService]
 })
 export class PaymentsModule {}
