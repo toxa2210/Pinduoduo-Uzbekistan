0870b4fe672701ad2f5d4cd2cc6f77102185e52c
toxa2210 <artikovtohir2210@gmail.com>
2026-09-24 19:39:29 +0500

    feat(backend): add backend/src/modules/catalog/catalog.module.ts

---
+++ backend/src/modules/catalog/catalog.module.ts
@@ -0,0 +1,1 @@
+import { Module } from "@nestjs/common"; import { CatalogController } from "./catalog.controller"; import { CatalogService } from "./catalog.service"; @Module({controllers:[CatalogController],providers:[CatalogService]}) export class CatalogModule {}
