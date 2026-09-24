a24a38fff4a950ab3155c47cfc0b55f404d91a62
toxa2210 <artikovtohir2210@gmail.com>
2026-09-24 20:24:21 +0500

    feat(paynet): register UWS integration

--- backend/src/modules/integrations/payments/payments.module.ts
+++ backend/src/modules/integrations/payments/payments.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { ClickService } from "./click.service";
 import { PaymeService } from "./payme.service";
 import { PaymentsController } from "./payments.controller";
+import { PaynetController } from "./paynet.controller";
+import { PaynetService } from "./paynet.service";
 
 @Module({
-  controllers: [PaymentsController],
-  providers: [ClickService, PaymeService],
-  exports: [ClickService, PaymeService]
+  controllers: [PaymentsController, PaynetController],
+  providers: [ClickService, PaymeService, PaynetService],
+  exports: [ClickService, PaymeService, PaynetService]
 })
 export class PaymentsModule {}
